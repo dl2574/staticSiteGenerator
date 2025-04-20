4da65c007e4cdba0d9cdc17d84a9af35516ebd76
dl2574 <20144415+dl2574@users.noreply.github.com>
2025-04-20 04:22:14 +0300

    Create LeafeNode and test cases

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_prop_string(self):
@@ -16,4 +16,17 @@ class TestHTMLNode(unittest.TestCase):
     def test_to_HTML_Error(self):
         node = HTMLNode("a", "A Link, WOW", props={"href":"https://www.hellowworld.com"})
         self.assertRaises(NotImplementedError, node.to_html)
-    
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "This Link", {"href":"www.google.com"})
+        self.assertEqual(node.to_html(), "<a href=\"www.google.com\">This Link</a>")
+
+    def test_leaf_to_html_b(self):
+        node = LeafNode("b", "BOLD TEXT")
+        self.assertEqual(node.to_html(), "<b>BOLD TEXT</b>")
