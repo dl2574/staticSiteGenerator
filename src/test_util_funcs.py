9fe5b2e54694be42b2522cc4c03e1e8c7d05aa96
dl2574 <20144415+dl2574@users.noreply.github.com>
2025-05-07 12:36:01 +0300

    Add text_to_textnode function and tests

--- src/test_util_funcs.py
+++ src/test_util_funcs.py
@@ -111,3 +111,23 @@ class TestSplitLinkNodes(unittest.TestCase):
             ],
             new_nodes,
         )
+
+class TestTextToTextNodes(unittest.TestCase):
+    def test_split_all(self):
+        base_text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        node_list = utils.text_to_textnodes(base_text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            node_list
+        )
--- src/utils.py
+++ src/utils.py
@@ -21,12 +21,15 @@ def text_node_to_html_node(text_node):
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_node_list = []
     for node in old_nodes:
-        split_node = node.text.split(delimiter)
-        for i, text in enumerate(split_node):
-            if i % 2 == 0:
-                new_node_list.append(TextNode(text,TextType.TEXT))
-            else:
-                new_node_list.append(TextNode(text, text_type))
+        if node.text_type == TextType.TEXT:
+            split_node = node.text.split(delimiter)
+            for i, text in enumerate(split_node):
+                if i % 2 == 0:
+                    new_node_list.append(TextNode(text,TextType.TEXT))
+                else:
+                    new_node_list.append(TextNode(text, text_type))
+        else:
+            new_node_list.append(node)
 
     return new_node_list
 
@@ -41,23 +44,26 @@ def extract_markdown_links(text):
 def split_nodes_image(old_nodes):
     new_node_list = []
     for node in old_nodes:
-        extracted_images = extract_markdown_images(node.text)
-        split_node = re.split(r"\!\[.*?\]\(.*?\)", node.text)
-        for text in split_node:
-            if text == "":
-                try:
-                    image = extracted_images.pop(0)
-                    new_node_list.append(TextNode(image[0], TextType.IMAGE, image[1]))
-                except IndexError:
-                    pass
+        if node.text_type == TextType.TEXT:
+            extracted_images = extract_markdown_images(node.text)
+            split_node = re.split(r"\!\[.*?\]\(.*?\)", node.text)
+            for text in split_node:
+                if text == "":
+                    try:
+                        image = extracted_images.pop(0)
+                        new_node_list.append(TextNode(image[0], TextType.IMAGE, image[1]))
+                    except IndexError:
+                        pass
 
-            else:
-                new_node_list.append(TextNode(text, TextType.TEXT))
-                try:
-                    image = extracted_images.pop(0)
-                    new_node_list.append(TextNode(image[0], TextType.IMAGE, image[1]))
-                except IndexError:
-                    pass
+                else:
+                    new_node_list.append(TextNode(text, TextType.TEXT))
+                    try:
+                        image = extracted_images.pop(0)
+                        new_node_list.append(TextNode(image[0], TextType.IMAGE, image[1]))
+                    except IndexError:
+                        pass
+        else:
+            new_node_list.append(node)
     return new_node_list
 
 
@@ -65,24 +71,33 @@ def split_nodes_image(old_nodes):
 def split_nodes_link(old_nodes):
     new_node_list = []
     for node in old_nodes:
-        extracted_links = extract_markdown_links(node.text)
-        split_node = re.split(r"\[.*?\]\(.*?\)", node.text)
-        for text in split_node:
-            if text == "":
-                try:
-                    link = extracted_links.pop(0)
-                    new_node_list.append(TextNode(link[0], TextType.LINK, link[1]))
-                except IndexError:
-                    pass
+        if node.text_type == TextType.TEXT:
+            extracted_links = extract_markdown_links(node.text)
+            split_node = re.split(r"\[.*?\]\(.*?\)", node.text)
+            for text in split_node:
+                if text == "":
+                    try:
+                        link = extracted_links.pop(0)
+                        new_node_list.append(TextNode(link[0], TextType.LINK, link[1]))
+                    except IndexError:
+                        pass
 
-            else:
-                new_node_list.append(TextNode(text, TextType.TEXT))
-                try:
-                    link = extracted_links.pop(0)
-                    new_node_list.append(TextNode(link[0], TextType.LINK, link[1]))
-                except IndexError:
-                    pass
+                else:
+                    new_node_list.append(TextNode(text, TextType.TEXT))
+                    try:
+                        link = extracted_links.pop(0)
+                        new_node_list.append(TextNode(link[0], TextType.LINK, link[1]))
+                    except IndexError:
+                        pass
+        else:
+            new_node_list.append(node)
     return new_node_list
 
 def text_to_textnodes(text):
-    pass
+    base_node = TextNode(text, TextType.TEXT)
+    bold_list = split_nodes_delimiter([base_node], "**", TextType.BOLD)
+    bold_italic_list = split_nodes_delimiter(bold_list, "_", TextType.ITALIC)
+    bold_italic_code_list = split_nodes_delimiter(bold_italic_list, "`", TextType.CODE)
+    bold_italic_code_image_list = split_nodes_image(bold_italic_code_list)
+    final_list = split_nodes_link(bold_italic_code_image_list)
+    return final_list
